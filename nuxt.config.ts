8142b00baa40d1c2c3b66d9833655c67695d07d5
liting <luz.liting@gmail.com>
2023-06-02 16:56:41 +0800

    chore: add modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  modules: [
+    '@nuxtjs/eslint-module',
+    '@unocss/nuxt',
+    '@nuxt/image-edge',
+    '@nuxthq/ui',
+    '@nuxtjs/color-mode',
+    '@nuxtjs/stylelint-module',
+    '@nuxtjs/fontaine',
+    '@nuxt/image-edge',
+    '@vueuse/nuxt',
+    'nuxt-lodash',
+  ],
+  colorMode: {
+    storageKey: 'nuxt-color-mode',
+  },
+  image: {
+    screens: {
+      sm: 676,
+      md: 960,
+      lg: 1440,
+    },
+    provider: 'ipx',
+  },
 })
---
+++ uno.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig, presetAttributify, presetIcons, presetTypography, presetUno, transformerDirectives, transformerVariantGroup } from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetAttributify(),
+    presetTypography(),
+    presetIcons({
+      extraProperties: {
+        'display': 'inline-block',
+        'vertical-align': 'middle',
+      },
+    }),
+    presetUno(),
+  ],
+  theme: {
+    breakpoints: {
+      sm: '480px',
+      md: '960px',
+      lg: '1440px',
+    },
+  },
+  transformers: [
+    transformerDirectives(),
+    transformerVariantGroup(),
+  ],
+})
